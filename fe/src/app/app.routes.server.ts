ae311aefafb334ae4f290790b01ed9b236cfa289
Vishal Surya <vishalsurya2701@gmail.com>
2026-03-31 22:51:18 +0530

    ssr route

--- fe/src/app/app.routes.server.ts
+++ fe/src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'edit/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
