b8cfda354b4f198f4f3c6935210267ec028f32a9
Vishal Surya <vishalsurya2701@gmail.com>
2026-03-31 21:22:15 +0530

    bakcend changes

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = 3000;
 
 app.use(cors());
-//enable json body parser
 app.use(express.json());
+
 const customerRoutes = require('./routes/customer');
 app.use('/api/customers', customerRoutes);
 
@@ -14,18 +14,17 @@ app.get('/', (req, res) => {
     res.send('Backend API is running');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
 connectToMongoDB();
 
 async function connectToMongoDB() {
-    const MONGODB_URI = 'mongodb+srv://vishalsurya27_db_user:0Wa7SMKOu1Bf9mg4@cluster-1.72zt3f5.mongodb.net/?appName=Cluster-1';
+    const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://vishalsurya27_db_user:0Wa7SMKOu1Bf9mg4@cluster-1.72zt3f5.mongodb.net/customersDB?appName=Cluster-1';
     try {
         await mongoose.connect(MONGODB_URI);
+        mongoose.set('strictQuery', true);
         console.log('Connected to MongoDB');
     } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
+        console.error('Error connecting to MongoDB:', error.message);
     }
 }
+
+module.exports = app;
